37a5f7cc687edf0cbc73008b5e33eb92d3ffeff2
Todd Turnsplenty <toddturnsplenty15@gmail.com>
2020-07-28 16:44:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction_with_language
-  puts "Hi, my name is and I am learning to prgram in."
+def introduction_with_language(name , language)
+  puts "Hi, my name is #{name} and I am learning to prgram in #{language}."
 end
